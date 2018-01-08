df501d6f9a7facc057e1620a297d3513c039abb3
Rafael Timbó <timbo.rafa@gmail.com>
2018-01-07 22:09:19 -0500

    healthy exam levels

--- src/app/@core/data/heartbit-api.service.ts
+++ src/app/@core/data/heartbit-api.service.ts
@@ -13,6 +13,36 @@ export class HeartbitApiService {
     //{ name: 'luce', age: 18, doctor: 'Smith', insurance: 'guardme', createdAt: Date.now().toString() , garbage: 'garbage'}
   ]
 
+  public patientId;
+
+  levels = {
+    glucose: {
+      min: 70,
+      max: 99,
+      unit: 'mg/dL'
+    },
+    redBloodCells: {
+      min: 4.3,
+      max: 5.7,
+      unit: 'mi/mm³'
+    },
+    whiteBloodCells: {
+      min: 3.5,
+      max: 10.5,
+      unit: 'mi/cm³'
+    },
+    platelet: {
+      min: 150,
+      max: 450,
+      unit: '/cm³'
+    },
+    iron: {
+      min: 49,
+      max: 181,
+      unit: 'ug/dL'
+    }
+  }
+
   constructor( private http: Http) {
    }
 
