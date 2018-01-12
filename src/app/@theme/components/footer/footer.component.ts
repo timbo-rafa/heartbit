5a2414fef463b1c721f18bcf5a2b87adad715b02
Rafael Timbó <timbo.rafa@gmail.com>
2018-01-12 13:50:45 -0500

    Customized nebular footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
-  template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2017</span>
+  template: 
+  `
+    <span class="created-by">Created by <b><a href="https://https://github.com/timbo-rafa/heartbit-web" target="_blank">Rafael, Sérgio and Irvinder</a></b> 2018</span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
