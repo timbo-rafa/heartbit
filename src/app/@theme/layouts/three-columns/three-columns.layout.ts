78544e808adf9f230b0d58123b0a9cace249214d
Rafael Timbó <timbo.rafa@gmail.com>
2018-03-05 16:26:03 -0500

    Github button to 'Github' string

--- src/app/@theme/layouts/three-columns/three-columns.layout.ts
+++ src/app/@theme/layouts/three-columns/three-columns.layout.ts
@@ -12,8 +12,8 @@ import { Component } from '@angular/core';
 
       <nb-sidebar class="menu-sidebar" tag="menu-sidebar" responsive >
         <nb-sidebar-header>
-          <a href="#" class="btn btn-hero-success main-btn">
-            <i class="ion ion-social-github"></i> <span>Support Us</span>
+          <a href="https://github.com/timbo-rafa/heartbit-web" class="btn btn-hero-success main-btn">
+            <i class="ion ion-social-github"></i> <span>Github</span>
           </a>
         </nb-sidebar-header>
         <ng-content select="nb-menu"></ng-content>
