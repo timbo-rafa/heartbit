ad581a46bd2b27631c24257a2c3eca426905e6dd
Rafael Timbó <timbo.rafa@gmail.com>
2018-03-29 11:43:50 -0400

    footer fix

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -7,10 +7,11 @@ import { Component } from '@angular/core';
   `
     <span class="created-by">Created by <b><a href="https://github.com/timbo-rafa/heartbit-web" target="_blank">Rafael and Sergio</a></b> 2018</span>
     <div class="socials">
+      <a href="https://linkedin.com/in/rtimbo" target="_blank" class="ion ion-social-linkedin"></a>
       <a href="https://github.com/timbo-rafa" target="_blank" class="ion ion-social-github"></a>
 <!--  <a href="#" target="_blank" class="ion ion-social-facebook"></a>
       <a href="#" target="_blank" class="ion ion-social-twitter"></a> -->
-      <a href="https://linkedin.com/in/rtimbo" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="https://www.linkedin.com/in/sergio-de-almeida-brunacci/" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
   `,
 })
