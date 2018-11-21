594a7353254f764b2418c910898b4c1127de686e
Rafael Timbó <timbo.rafa@gmail.com>
2018-11-21 14:29:42 -0500

    Cleanup: commented code

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,113 +1,6 @@
 import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
-  /*{
-    title: 'Dashboard',
-    icon: 'nb-home',
-    link: '/pages/dashboard',
-    home: true,
-  },
-  {
-    title: 'Patients',
-    icon: 'nb-person',
-    children: [
-      {
-        title: 'List Patients',
-        icon: 'ion-person-stalker',
-        link: '/pages/patients/list-patients',
-      },
-      {
-        title: 'Add Patient',
-        icon: 'ion-person-add',
-        link: '/pages/patients/add-patients',
-      },
-      {
-        title: 'View Patients',
-        icon: 'ion-person',
-        link: '/pages/patients/view-patients',
-      },
-    ]},
-  {
-    title: 'UI Features',
-    icon: 'nb-keypad',
-    link: '/pages/ui-features',
-    children: [
-      {
-        title: 'Buttons',
-        link: '/pages/ui-features/buttons',
-      },
-      {
-        title: 'Grid',
-        link: '/pages/ui-features/grid',
-      },
-      {
-        title: 'Icons',
-        link: '/pages/ui-features/icons',
-      },
-      {
-        title: 'Modals',
-        link: '/pages/ui-features/modals',
-      },
-      {
-        title: 'Typography',
-        link: '/pages/ui-features/typography',
-      },
-      {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields',
-      },
-      {
-        title: 'Tabs',
-        link: '/pages/ui-features/tabs',
-      },
-    ],
-  },
-  {
-    title: 'Forms',
-    icon: 'nb-compose',
-    children: [
-      {
-        title: 'Form Inputs',
-        link: '/pages/forms/inputs',
-      },
-      {
-        title: 'Form Layouts',
-        link: '/pages/forms/layouts',
-      },
-    ],
-  },
-  {
-    title: 'Components',
-    icon: 'nb-gear',
-    children: [
-      {
-        title: 'Tree',
-        link: '/pages/components/tree',
-      }, {
-        title: 'Notifications',
-        link: '/pages/components/notifications',
-      },
-    ],
-  },
-  {
-    title: 'Maps',
-    icon: 'nb-location',
-    children: [
-      {
-        title: 'Google Maps',
-        link: '/pages/maps/gmaps',
-      },
-      {
-        title: 'Leaflet Maps',
-        link: '/pages/maps/leaflet',
-      },
-      {
-        title: 'Bubble Maps',
-        link: '/pages/maps/bubble',
-      },
-    ],
-  },
-  */
   {
     title: 'Charts',
     icon: 'nb-bar-chart',
@@ -115,35 +8,9 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Patient\'s Record',
         link: '/pages/charts/echart/',
-      },
-      /*
-      {
-        title: 'Charts.js',
-        link: '/pages/charts/chartjs',
-      },
-      {
-        title: 'D3',
-        link: '/pages/charts/d3',
-      },
-      */
+      }
     ],
   },
-  /*
-  {
-    title: 'Editors',
-    icon: 'nb-title',
-    children: [
-      {
-        title: 'TinyMCE',
-        link: '/pages/editors/tinymce',
-      },
-      {
-        title: 'CKEditor',
-        link: '/pages/editors/ckeditor',
-      },
-    ],
-  },
-  */
   {
     title: 'Tables',
     icon: 'nb-tables',
